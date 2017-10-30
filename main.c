877d0389938b3c9a3548ad5de248319d5a577894
Tommy Hu <tomyhu1995@gmail.com>
2017-10-30 23:49:43 +0800

    modify main

--- main.c
+++ main.c
@@ -3,64 +3,11 @@
 #include <limits.h>
 #define INFINITY 9999
 #define MAX 10
- 
-void dijkstra(int G[MAX][MAX],int n,int startnode);
 
-float link_cost_calculate(int link_capacity, int link_length){
-    return 1.0;
-}
+#define TenMbps_cost 100.0
+#define OneHoudardMbps_cost 19
+#define OneGigaMbps_cost 4
 
-void initial_adjacency_matrix(int G[MAX][MAX]){
-    int i, j;
-
-    for(i=0;i < MAX;i++){
-        for(j=0;j < MAX;j++){
-            G[i][j] = INT_MAX;   
-        }
-    }
-}
- 
-int main()
-{
-    int G[MAX][MAX],i,j,n,u;
-    initial_adjacency_matrix(G);
-    printf("Enter no. of nodes:");
-    scanf("%d",&n);
-    
-
-    for(i = 0; i < n; i++){
-        int tmp, link_capacity, link_length;
-        printf("Please enter which nodes link to %d\n", i+1);
-        printf("Input can be terminated by entering 0\n");
-
-        while(printf("Node number(1~%d): ", n)){
-
-            if(scanf("%d", &tmp) && tmp != 0){
-                printf("link capacity: ");
-                scanf("%d", &link_capacity);
-                printf("link length: ");
-                scanf("%d", &link_length);
-                G[i][tmp-1] = link_cost_calculate(link_capacity, link_length);
-            }else{
-                break;
-            }
-        }
-    }
-
-    /*printf("\nprint the adjacency matrix:\n");
-    
-    for(i=0;i<n;i++)
-        for(j=0;j<n;j++)
-            printf("%d",G[i][j]);
-    */
-    
-    printf("\nEnter the starting node(1~%d): ", n);
-    scanf("%d",&u);
-    dijkstra(G,n,u-1);
-    
-    return 0;
-}
- 
 void dijkstra(int G[MAX][MAX],int n,int startnode)
 {
  
@@ -127,4 +74,76 @@ void dijkstra(int G[MAX][MAX],int n,int startnode)
                 printf("<-%d",j+1);
             }while(j!=startnode);
     }
+}
+
+int link_cost_calculate(int link_capacity){
+    int link_cost = 0;
+
+    switch(link_capacity){
+        case 10:
+            link_cost += TenMbps_cost;
+            break;
+        case 100:
+            link_cost += OneHoudardMbps_cost;
+            break;
+        case 1000:
+            link_cost += OneGigaMbps_cost;
+            break;
+        default:
+            break;
+    }
+
+
+    return link_cost;
+}
+
+void initial_adjacency_matrix(int G[MAX][MAX]){
+    int i, j;
+
+    for(i=0;i < MAX;i++){
+        for(j=0;j < MAX;j++){
+            G[i][j] = INT_MAX;   
+        }
+    }
+}
+ 
+int main()
+{
+    int G[MAX][MAX],i,j,n,start_node, end_node;
+    initial_adjacency_matrix(G);
+    printf("Enter no. of nodes:");
+    scanf("%d",&n);
+    
+
+    for(i = 0; i < n; i++){
+        int tmp, link_capacity, link_length;
+        printf("Please enter which nodes link to %d\n", i+1);
+        printf("Input can be terminated by entering 0\n");
+
+        while(printf("Node number(1~%d): ", n)){
+
+            if(scanf("%d", &tmp) && tmp != 0){
+                printf("link capacity(10 - 10Mbps, 100 - 100Mpbs, 1000 - 1Gbps): ");
+                scanf("%d", &link_capacity);
+                G[i][tmp-1] = link_cost_calculate(link_capacity);
+            }else{
+                break;
+            }
+        }
+    }
+
+    /*printf("\nprint the adjacency matrix:\n");
+    
+    for(i=0;i<n;i++)
+        for(j=0;j<n;j++)
+            printf("%d",G[i][j]);
+    */
+    
+    printf("\nEnter flow's start node(1~%d): ", n);
+    scanf("%d", &start_node);
+    printf("\nEnter flow's end node(1~%d): ", n);
+    scanf("%d", &end_node);
+    dijkstra(G,n,start_node-1);
+    
+    return 0;
 }
